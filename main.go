6a2183c6cc9a2cad672c42ae5d64fcd5e8d990ce
nadhifmf <nadhifmmff@gmail.com>
2022-08-08 11:05:43 +0700

    Bug fixed

--- main.go
+++ main.go
@@ -1,28 +1,33 @@
 package main
 
 import(
-	"io/ioutil"
+	//"io/ioutil"
 	"log"
 	//"errors"
 	"net/http"
 	"github.com/gin-gonic/gin"
+	"encoding/json"
 )
 
 
 func getLokalTerdaftar(context *gin.Context){
-	resp, err := http.Get("https://pse.kominfo.go.id/static/json-static/LOKAL_TERDAFTAR/0.json?page[page]=1.json?page[page]=")
+	url := "https://pse.kominfo.go.id/static/json-static/LOKAL_TERDAFTAR/0.json?page[page]=1.json?page[page]="
+	resp, err := http.Get(url)
 	if err != nil {
 		log.Fatalln(err)
 	}
+	defer resp.Body.Close()
+	var j interface{}
+    err = json.NewDecoder(resp.Body).Decode(&j)
 	//We Read the response body on the line below.
-	body, err := ioutil.ReadAll(resp.Body)
+	//body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatalln(err)
 	}
 	//Convert the body to type string
-	sb := string(body)
-	log.Printf(sb)
-	context.IndentedJSON(http.StatusOK, sb)
+	//sb := string("%s", j)
+	//log.Printf(sb)
+	context.IndentedJSON(http.StatusOK, j)
 }
 
 func main()  {
