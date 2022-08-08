594f5fa3a4a73f6edacf113650eb280a778728c4
nadhifmf <nadhifmmff@gmail.com>
2022-08-08 10:11:05 +0700

    Error fixed

--- main.go
+++ main.go
@@ -3,23 +3,30 @@ package main
 import(
 	"io/ioutil"
 	"log"
+	//"errors"
 	"net/http"
 	"github.com/gin-gonic/gin"
 )
 
 
-func main()  {
+func getLokalTerdaftar(context *gin.Context){
 	resp, err := http.Get("https://pse.kominfo.go.id/static/json-static/LOKAL_TERDAFTAR/0.json?page[page]=1.json?page[page]=")
 	if err != nil {
-	   log.Fatalln(err)
+		log.Fatalln(err)
 	}
- //We Read the response body on the line below.
+	//We Read the response body on the line below.
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-	   log.Fatalln(err)
+		log.Fatalln(err)
 	}
- //Convert the body to type string
+	//Convert the body to type string
 	sb := string(body)
 	log.Printf(sb)
+	context.IndentedJSON(http.StatusOK, sb)
+}
+
+func main()  {
 	router := gin.Default()
+	router.GET("/pselokal", getLokalTerdaftar)
+	router.Run("localhost:9011")
 }
